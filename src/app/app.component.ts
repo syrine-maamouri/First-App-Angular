44cbc7ba97cabd13c92340a8fbe637a3a9c492c7
Syrine <syrine.maamouri92@gmail.com>
2019-10-04 09:19:23 +0100

    first app angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-project';
+  count =1;
+ 
+ listProducts :Array<any>=[];
+ 
+
+  /*countValue(mytext:string){
+
+    let v= Number.parseInt(mytext);
+    this.count += v;
+
+  }*/
+
+  calcul(name:string,price=0){
+
+   price = price*0.18;
+   this.listProducts.push({name,price});
+   console.log(this.listProducts)
+
+  }
+
+  filterProducts(event){
+
+    console.log("event",event.target.value)
+  
+     this.listProducts.filter(x=>{
+       
+      return (x.name !== '' && x.price !== 0) ;
+
+    });
+
+
+  }
+
 }
